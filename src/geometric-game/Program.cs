27989bd880c8a2602a4feec89a7eec01eba3ad5d
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-13 20:06:06 -0300

    :sparkles: feat: implementing in the main method, the call of the method to generate another geometric shape, where it will ask if the user wants to transform the generated diamond into a triangle, if so the method will be called

--- src/geometric-game/Program.cs
+++ src/geometric-game/Program.cs
@@ -7,7 +7,23 @@ public class Program
     Console.WriteLine("Digite uma letra de A a Z");
     var letter = Console.ReadLine();
 
+    if (letter.Length > 1)
+    {
+      Console.WriteLine("Você digitou mais de uma letra, por favor digite apenas uma letra de A a Z");
+      letter = Console.ReadLine();
+    }
     DiamondGame.GenerateDiamond(letter);
 
+    Console.WriteLine("Você deseja transformar o diamante em um triangulo? S/N");
+    var transform = Console.ReadLine();
+
+    if (transform == "S" || transform == "s")
+    {
+      Triangle.TransformTriangle(letter);
+    }
+    else
+    {
+      Console.WriteLine("Obrigado por utilizar o programa!");
+    }
   }
 }
