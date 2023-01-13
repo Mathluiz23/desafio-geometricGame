3c4a20a4ad7623d32d61f4c67838691d0d7152ad
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-13 19:35:08 -0300

    :sparkles: feat: Implementing the top part of the diamond drawing, where the index will be captured in the alphabetic string and according to the informed letter, it will be traversed and filled in each line with the previous letters of the alphabet and a space between them generated

---
+++ src/geometric-game/DiamondGame.cs
@@ -0,0 +1,34 @@
+
+namespace diamond_game;
+public class DiamondGame
+{
+
+  public static void GenerateDiamond(string letter)
+  {
+    letter = letter.ToUpper();
+
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var letterIndex = alphabet.IndexOf(letter);
+
+
+    var diamond = "";
+
+    for (var i = 0; i <= letterIndex; i++)
+    {
+      var line = new string(' ', letterIndex - i);
+      line += alphabet[i];
+      if (i > 0)
+      {
+        line += new string(' ', i * 2 - 1);
+        line += alphabet[i];
+      }
+      diamond = diamond + line + Environment.NewLine;
+      Console.WriteLine(line);
+    }
+
+    Console.WriteLine("Sua figura é um diamante");
+  }
+}
+
+
+
