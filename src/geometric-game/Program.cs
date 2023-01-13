3d5f108cc59e6cb28a8d07af4ccd8a092202e582
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-13 19:29:19 -0300

    :sparkles: feat: Implementing the Main method, it will ask the user to inform a letter of the alphabet, after that this parameter will be sent to the GenerateDiamond method where a diamond figure will be formed according to the informed letter

---
+++ src/geometric-game/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace diamond_game;
+public class Program
+{
+
+  public static void Main()
+  {
+    Console.WriteLine("Digite uma letra de A a Z");
+    var letter = Console.ReadLine();
+
+    DiamondGame.GenerateDiamond(letter);
+
+  }
+}
