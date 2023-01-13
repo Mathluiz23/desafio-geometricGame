e8eac8ad0e2dd3c30e48b86bd16282e3078f7b94
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-13 19:38:22 -0300

    :sparkles: feat: Replicating reversed logic to complete the other half of the diamond

--- src/geometric-game/DiamondGame.cs
+++ src/geometric-game/DiamondGame.cs
@@ -8,6 +8,7 @@ public class DiamondGame
     letter = letter.ToUpper();
 
     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     var letterIndex = alphabet.IndexOf(letter);
 
 
@@ -25,7 +26,19 @@ public class DiamondGame
       diamond = diamond + line + Environment.NewLine;
       Console.WriteLine(line);
     }
+    for (var i = letterIndex - 1; i >= 0; i--)
+    {
+      var line = new string(' ', letterIndex - i);
+      line += alphabet[i];
+      if (i > 0)
+      {
+        line += new string(' ', i * 2 - 1);
+        line += alphabet[i];
+      }
+      diamond = diamond + line + Environment.NewLine;
+      Console.WriteLine(line);
 
+    }
     Console.WriteLine("Sua figura é um diamante");
   }
 }
