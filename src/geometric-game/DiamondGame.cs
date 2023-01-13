de6cf154902e919773fb0732f71571b3f1f5da51
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-13 19:41:22 -0300

    sparkles: feat: Implementing logic to check if the letter entered by the user is equal to A or B, if so, a new method will be invoked to draw the diamond by repeating these letters

--- src/geometric-game/DiamondGame.cs
+++ src/geometric-game/DiamondGame.cs
@@ -8,9 +8,17 @@ public class DiamondGame
     letter = letter.ToUpper();
 
     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int letterIndex = 0;
 
-    var letterIndex = alphabet.IndexOf(letter);
+    if (letter == "A" || letter == "B")
+    {
+      DiamondRepeatLetter(letter);
+    }
 
+    else
+    {
+      letterIndex = alphabet.IndexOf(letter);
+    }
 
     var diamond = "";
 
@@ -40,6 +48,72 @@ public class DiamondGame
 
     }
     Console.WriteLine("Sua figura é um diamante");
+    Report.GenerateReport(diamond, letter);
+  }
+
+  private static void DiamondRepeatLetter(string letter)
+
+  {
+    var diamond = "";
+    if (letter == "A")
+    {
+      for (var i = 0; i <= 2; i++)
+      {
+        var line = new string(' ', 2 - i);
+        line += "A";
+        if (i > 0)
+        {
+          line += new string(' ', i * 2 - 1);
+          line += "A";
+        }
+        diamond = diamond + line + Environment.NewLine;
+        Console.WriteLine(line);
+      }
+
+      for (var i = 1; i >= 0; i--)
+      {
+        var line = new string(' ', 2 - i);
+        line += "A";
+        if (i > 0)
+        {
+          line += new string(' ', i * 2 - 1);
+          line += "A";
+        }
+        diamond = diamond + line + Environment.NewLine;
+
+        Console.WriteLine(line);
+      }
+    }
+    else
+    {
+      for (var i = 0; i <= 3; i++)
+      {
+        var line = new string(' ', 3 - i);
+        line += "B";
+        if (i > 0)
+        {
+          line += new string(' ', i * 2 - 1);
+          line += "B";
+        }
+        diamond = diamond + line + Environment.NewLine;
+        Console.WriteLine(line);
+      }
+
+      for (var i = 2; i >= 0; i--)
+      {
+        var line = new string(' ', 3 - i);
+        line += "B";
+        if (i > 0)
+        {
+          line += new string(' ', i * 2 - 1);
+          line += "B";
+        }
+        diamond = diamond + line + Environment.NewLine;
+        Console.WriteLine(line);
+      }
+    }
+    Console.WriteLine("Sua figura é um diamante");
+    Report.GenerateReport(diamond, letter);
   }
 }
 
